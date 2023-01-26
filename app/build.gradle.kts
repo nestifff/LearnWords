20f8091357aa7967a3a71d4806c82d6bf63f4656
Svintilova Anastasiya <anastasiya.svintilova@lemellabs.com>
2023-01-26 16:36:58 +0300

    Add signingConfigs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,21 @@ android {
     namespace = "com.nestifff.learnwords"
     compileSdk = AppBuildConfig.CompileSDK
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("words.jks")
+            storePassword = "wordswords"
+            keyAlias = "words"
+            keyPassword = "wordswords"
+        }
+        create("release") {
+            storeFile = file("words.jks")
+            storePassword = "wordswords"
+            keyAlias = "words"
+            keyPassword = "wordswords"
+        }
+    }
+
     defaultConfig {
         applicationId = AppBuildConfig.PackageName
         minSdk = AppBuildConfig.MinSDK
@@ -30,6 +45,8 @@ android {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
             isDebuggable = false
+
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
@@ -58,19 +75,24 @@ dependencies {
 
     implementation(Dependencies.App.Core.corKtx)
     implementation(Dependencies.App.Core.activityCompose)
+
     implementation(Dependencies.App.Lifecycle.lifecycleKtx)
     implementation(Dependencies.App.Lifecycle.viewModelCompose)
+
     implementation(Dependencies.App.Compose.ui)
     implementation(Dependencies.App.Compose.tooling)
     implementation(Dependencies.App.Compose.material)
+
     implementation(Dependencies.App.Accompanist.animatedNavigation)
+    implementation(Dependencies.App.Accompanist.uiController)
+
     testImplementation(Dependencies.Common.Test.jUnit)
     androidTestImplementation(Dependencies.Common.Test.androidJUnit)
     androidTestImplementation(Dependencies.Common.Test.espresso)
+
     androidTestImplementation(Dependencies.App.Compose.uiTest)
     debugImplementation(Dependencies.App.Compose.toolingTest)
     debugImplementation(Dependencies.App.Compose.manifestTest)
-    debugImplementation(Dependencies.App.Accompanist.uiController)
 
     implementation(Dependencies.Common.Dagger.dagger)
     kapt(Dependencies.Common.Dagger.compiler)
