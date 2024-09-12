978427490f4d421e2de2885d2e8f798108a7dd73
nestifff <nestiv.01.12@gmail.com>
2024-09-13 01:54:36 +0300

    add applicationIdSuffix for debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
         getByName("debug") {
             isMinifyEnabled = false
             isDebuggable = true
+            applicationIdSuffix = ".debug"
         }
         getByName("release") {
             isMinifyEnabled = false
